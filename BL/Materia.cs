8f9b534ab95b1b43f6100f0262b3899ca9685155
digis <digis@DESKTOP-NU5U73O>
2025-02-11 17:14:24 -0600

    Add project files.

---
+++ BL/Materia.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Materia
+    {
+        public static void Add(ML.Materia materia)
+        {
+            //1 abrir una conexion con la BD
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Connection.GetConnection()))
+                {
+                    string query = "INSERT INTO Materia VALUES (@nombre, @creditos, @costo)";
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = query; //query que va a ejecutar
+                    cmd.Connection = context; //BD
+
+                    cmd.Parameters.AddWithValue("@nombre", materia.Nombre);
+                    cmd.Parameters.AddWithValue("@creditos", materia.Creditos);
+                    cmd.Parameters.AddWithValue("@costo", materia.Costo);
+
+                    context.Open(); //abrir la conexion con la BD
+                    int filasAfectadas = cmd.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+                    {
+                        Console.WriteLine("EL registro se inserto correctamente");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error al insertar");
+                    }
+                }
+                //CERRAR
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error de conexion");
+            }
+        }
+        public static void Delete(int idMateria)
+        {
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Connection.GetConnection()))
+                {
+                    string query = "DELETE FROM Materia WHERE IdMateria = @IdMateria";
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = query;
+                    cmd.Connection = context;
+
+                    cmd.Parameters.AddWithValue("@IdMateria", idMateria);
+
+                    context.Open();
+                    int filasAfectadas = cmd.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+                    {
+                        Console.WriteLine("El registro se ha eliminado");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error al eliminar");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error de conexion");
+            }
+        }
+
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Connection.GetConnection()))
+                {
+                    string query = "SELECT * FROM Materia";
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = query;
+                    cmd.Connection = context;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        //si trae registros
+                        result.Objects = new List<object>();
+
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            ML.Materia materia = new ML.Materia();
+                            materia.IdMateria = Convert.ToInt32(row[0].ToString());
+                            materia.Nombre = row[1].ToString();
+                            materia.Creditos = Convert.ToDecimal(row[2].ToString());
+                            materia.Costo = Convert.ToDecimal(row[3].ToString());
+
+                            result.Objects.Add(materia);
+                        }
+                        result.Correct = true;
+
+                    }
+                    else
+                    {
+                        //no hay registros
+                        result.Correct = false;
+                        result.ErrorMessage = "No hay datos/registros";
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //HUBO UN ERROR
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+
+            return result;
+        }
+
+        public static ML.Result GetById(int IdMateria)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Connection.GetConnection()))
+                {
+                    string query = "SELECT * FROM Materia WHERE IdMateria = @IdMateria";
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = query;
+                    cmd.Connection = context;
+
+                    cmd.Parameters.AddWithValue("@IdMateria", IdMateria);
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        DataRow row = dataTable.Rows[0];
+
+                        ML.Materia materia = new ML.Materia();
+                        materia.IdMateria = Convert.ToInt32(row[0].ToString());
+                        materia.Nombre = row[1].ToString();
+                        materia.Creditos = Convert.ToDecimal(row[2].ToString());
+                        materia.Costo = Convert.ToDecimal(row[3].ToString());
+
+                        result.Object = materia; //BOXING
+
+                        result.Correct = true;
+
+                    }
+                    else
+                    {
+                        //no hay registros
+                        result.Correct = false;
+                        result.ErrorMessage = "No hay datos/registros";
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //HUBO UN ERROR
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PL/Materia.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Materia
+    {
+        public static void Add() //logica para pedir la informacion
+        {
+            ML.Materia materia = new ML.Materia();
+
+            Console.WriteLine("Dame el nombre de la materia");
+            materia.Nombre = Console.ReadLine();
+            Console.WriteLine("Dame los creditos");
+            materia.Creditos = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Dame el costo");
+            materia.Costo = Convert.ToDecimal(Console.ReadLine());
+
+            BL.Materia.Add(materia);
+        }
+        public static void Delete() //logica para pedir la informacion
+        {
+
+            Console.WriteLine("Dame el id de la materia que quieres eliminar");
+            int idMateria = Convert.ToInt32(Console.ReadLine());
+
+            BL.Materia.Delete(idMateria);
+        }
+        public static void GetAll()
+        {
+            ML.Result result = BL.Materia.GetAll();
+
+            if (result.Correct)
+            {
+                //mostrar los registros
+                foreach(ML.Materia materia in result.Objects)
+                {
+                    Console.WriteLine(materia.IdMateria);
+                    Console.WriteLine(materia.Creditos);
+                }
+            } else
+            {
+                Console.WriteLine("Hubo un error "+result.ErrorMessage);
+            }
+        }
+        public static void GetById() //logica para pedir la informacion
+        {
+            Console.WriteLine("Dame el id que quieres buscar");
+            int idMateria = Convert.ToInt32(Console.ReadLine());
+
+            ML.Result result = BL.Materia.GetById(idMateria);
+
+            if (result.Correct)
+            {
+                //imprimir la informacion de la materia
+                ML.Materia materia = (ML.Materia)result.Object; //UNBOXING
+                Console.WriteLine(materia.Nombre);
+                Console.WriteLine(materia.Creditos);
+                Console.WriteLine(materia.Costo);
+            } else
+            {
+                Console.WriteLine("error " + result.ErrorMessage);
+            }
+        }
+    }
+}
